00d593c7a6196eeda6a22fdc110cfabca0f5c69d
k3yboardnerd <58972426+k3yboardnerd@users.noreply.github.com>
2023-12-02 00:38:52 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,6 +6,13 @@ export const protect = asyncHandler(async (req, res, next) => {
   let token
   token = req.cookies.jwt
 
+  if (!token) {
+    const authorization = req.header("Authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+      token = authorization.split(" ")[1]; // Extract token from header
+    }
+  }
+  
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
