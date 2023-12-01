85d485b59c6f2c093e36eccfd518b49df771bee1
k3yboardnerd <58972426+k3yboardnerd@users.noreply.github.com>
2023-12-02 01:28:57 +0200

    Update cors

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(cors({
   origin: 'https://kbnblog.onrender.com',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Set-Cookie'],
   credentials: true,
 }))
 app.use('/api/users', userRoutes)
